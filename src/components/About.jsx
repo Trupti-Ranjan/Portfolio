132e4d4a133a45ab8820eb9766bd25f97936c00b
Trupti-Ranjan <adarshbarik563@gmail.com>
2025-08-24 12:38:33 +0530

    Complete About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import aboutme from '../assets/images/aboutme.svg'
-import Square from './ui/Square';
+import aboutme from '../assets/images/aboutme.svg';
+import Dot5X5 from './ui/Dot5X5';
 
 const About = () => {
   return (
-    <section className="mx-auto p-4 sm:flex sm:max-w-[1200px] sm:flex-col sm:px-0">
+    <section className="mx-auto mb-10 p-4 sm:flex sm:max-w-[1200px] sm:flex-col sm:px-0">
       {/* Title Section of About */}
       <div className="mb-10 flex w-full flex-col items-start justify-center gap-5 md:flex-row md:items-center md:justify-start">
         <h2 className="text-[28px] text-white md:text-[32px]">
@@ -13,24 +13,67 @@ const About = () => {
         <div className="bg-primary h-[1px] w-[240px] md:w-[239px]"></div>
       </div>
 
-      <div className='flex'>
-        <div className='w-[525px]'>
-          <p className='text-gray'>
-            Hello, i’m Trupti!<br /> <br />I’m a self-taught front-end developer based in
-            Odisha, India. I can develop responsive websites from scratch and
-            raise them into modern user-friendly web experiences.<br /> <br />Transforming
-            my creativity and knowledge into a websites has been my passion for
-            over a year. I have been helping various clients to establish their
-            presence online. I always strive to learn about the newest
-            technologies and frameworks.
+      <div className="relative flex flex-col-reverse items-center gap-12 md:flex-row md:justify-between md:gap-0">
+        <div className="md:w-[530px]">
+          <p className="text-gray">
+            Hello, I’m{' '}
+            <span className="text-primary font-semibold">Trupti</span>!
+            <br /> <br />
+            I’m a{' '}
+            <span className="text-primary font-semibold">
+              self-taught front-end developer {" "}
+            </span>
+            based in{' '}
+            <span className="text-primary font-semibold">Odisha, India</span>. I
+            can develop{' '}
+            <span className="text-primary font-semibold">
+              responsive websites {" "}
+            </span>
+            from scratch and raise them into
+            <span className="text-primary font-semibold">
+              {' '}
+              modern, user-friendly web experiences
+            </span>
+            .
+            <br /> <br />
+            Transforming my{' '}
+            <span className="text-primary font-semibold">creativity</span> and
+            <span className="text-primary font-semibold"> knowledge</span> into
+            <span className="text-primary font-semibold"> websites</span> has
+            been my passion for over a year. I have been helping{' '}
+            <span className="text-primary font-semibold">
+              clients establish their presence online
+            </span>
+            . I always strive to learn about the
+            <span className="text-primary font-semibold">
+              {' '}
+              newest technologies and frameworks
+            </span>
+            .
           </p>
-          <button className='px-4 py-2 mt-8 text-white border border-primary'>Read More -{">"}</button>
+
+          <button className="border-primary mt-8 border px-4 py-2 text-white hover:cursor-pointer">
+            Read More -{'>'}
+          </button>
         </div>
-        <div>
-          <img src={aboutme} alt="About Me" loading='lazy'/>
-          <span>
-            <Square />
+        <div className="-top-28 right-1 md:absolute">
+          <img
+            src={aboutme}
+            alt="About Me"
+            loading="lazy"
+            className="h-[300px] md:h-[480px]"
+          />
+          <div className="bg-primary h-[1px] w-[200px] md:w-[360px]"></div>
+          <span className="absolute top-1 left-16 md:top-16 md:left-3">
+            <Dot5X5 />
           </span>
+          <span className="absolute top-48 right-16 md:top-70 md:left-52">
+            <Dot5X5 rows={4} cols={5} gap={12} width={90} />
+          </span>
+          <span className="absolute top-80 -right-46 hidden md:block">
+            <Dot5X5 rows={5} cols={5} gap={18} width={100} />
+          </span>
+          <span></span>
         </div>
       </div>
     </section>
